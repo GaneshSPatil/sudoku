6d6bb8665ab8b8382226ff5b6f890041929b46fc
ganeshpl <ganespl@INganespl.local>
2015-11-16 13:50:25 +0530

    test added in match_board_test.js for match_board function

--- match_board_test.js
+++ match_board_test.js
@@ -1,4 +1,6 @@
 var match_board = require('./match_board_test.js').match_board;
+var generate_board = require('./generate_board.js').generate_board;
+var expect = require('chai').expect;
 
 //-----------------------------------------------------------------------------------------------------------------------//
 
@@ -7,16 +9,21 @@ var match_board = require('./match_board_test.js').match_board;
 
 //-----------------------------------------------------------------------------------------------------------------------//
 
-test['match_board should return a boolean value'] = function(){
+var board = generate_board();
+var another_board = generate_board();
 
+//-----------------------------------------------------------------------------------------------------------------------//
+
+test['match_board should return a boolean value'] = function(){
+	expect(match_board(board, board)).to.be.a('boolean');
 };
 
 test['match_board should return true if both the boards are equal'] = function(){
-
+	expect(match_board(board, board)).to.equal(true);
 };
 
 test['match_board should return false if both the boards are not equal'] = function(){
-
+	expect(match_board(board, another_board)).to.equal(false);
 };
 
 //-----------------------------------------------------------------------------------------------------------------------//
