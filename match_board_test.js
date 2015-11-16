44143b667f03a57cbe8772548c9ae6eb23137f13
ganeshpl <ganespl@INganespl.local>
2015-11-16 13:43:38 +0530

    a templete of tests added in match_board_tests.js

---
+++ match_board_test.js
@@ -0,0 +1,22 @@
+var match_board = require('./match_board_test.js').match_board;
+
+//-----------------------------------------------------------------------------------------------------------------------//
+
+ var test = {};
+ exports.test = test;
+
+//-----------------------------------------------------------------------------------------------------------------------//
+
+test['match_board should return a boolean value'] = function(){
+
+};
+
+test['match_board should return true if both the boards are equal'] = function(){
+
+};
+
+test['match_board should return false if both the boards are not equal'] = function(){
+
+};
+
+//-----------------------------------------------------------------------------------------------------------------------//
