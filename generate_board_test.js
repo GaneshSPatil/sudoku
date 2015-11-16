42ff9ffcd045262117f7181050eb7601a30013bd
ganeshpl <ganespl@INganespl.local>
2015-11-16 12:28:33 +0530

    added the templete of tests in generate_board_test.js for generate_board function

---
+++ generate_board_test.js
@@ -0,0 +1,42 @@
+var generate_board = require('./generate_board.js').generate_board;
+var expect = require('chai').expect;
+
+//-----------------------------------------------------------------------------------------------------------------//
+
+var test = {};
+exports.test = test;
+
+//-----------------------------------------------------------------------------------------------------------------//
+
+console.log(generate_board);
+
+test['generate_board should return an array'] = function(){
+
+};
+
+test['generate_board should return an array of 9x9'] = function(){
+
+};
+
+test['generate_board should return an array which contains 9 elements and the type of elements should be array'] = function(){
+
+};
+
+test['generate_board should return an array in which each element of the array (subArray) should contains 9 elements'] = function(){
+
+};
+
+test['generate_board should return a board in which all elements in a row should be unique'] = function(){
+
+};
+
+test['generate_board should return a board in which all elements in a column should be unique'] = function(){
+
+};
+
+test['generate_board should return a board in which all elements in a grid should be unique'] = function(){
+
+};
+
+//-----------------------------------------------------------------------------------------------------------------//
+
